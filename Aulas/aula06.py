67d0b0ce839e546b95725f86da3aef5248877689
Nunu do Pinguim pingo!!! <tasinasso.emanuelli@escola.pr.gov.br>
2026-05-28 12:46:10 +0000

    Aula 06 - Análise quantitativa, usando histograma

--- Aulas/aula06.py
+++ Aulas/aula06.py
@@ -17,8 +17,31 @@ print(frequencia)
 frequencia.plot(kind="bar")
 
 plt.title("Frutas Preferidas dos Alunos")
-plt.Xlabel("Frutas")
+plt.xlabel("Frutas")
 plt.ylabel("Frequencia")
 
 plt.show()
 plt.savefig("aula06-qualitativo")
+
+plt.clf()
+
+# Análise quantitativa
+notas = [
+    5, 6, 7, 8, 10,
+    6, 5, 9, 7, 8,
+    5, 6, 7, 8, 9
+]
+
+serie = pd.Series(notas)
+frequencia = serie.value_counts()
+
+print(frequencia)
+
+serie.plot(kind="hist")
+
+plt.title("Distribuição das notas")
+plt.xlabel('Notas')
+plt.ylabel("Frequência")
+
+plt.show()
+plt.savefig("aula06-quantitativo")
